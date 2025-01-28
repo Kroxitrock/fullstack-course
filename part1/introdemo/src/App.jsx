79081c655ceca4945af985bf5224477964138c89
Andrey Strinski <kroxitrock@gmail.com>
2025-01-28 17:53:14 +0200

    Ex 1.2

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -15,8 +15,12 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
       />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
