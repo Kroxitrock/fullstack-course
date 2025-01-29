92fed81c9425860a0bfe50067d0f3d445cd32264
Andrey Strinski <kroxitrock@gmail.com>
2025-01-29 11:28:05 +0200

    Ex 1.11

--- part1/feedback-app/src/Statistics.jsx
+++ part1/feedback-app/src/Statistics.jsx
@@ -4,17 +4,23 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
 
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-
-      <StatisticLine text="all" value={good + neutral + bad} />
-      <StatisticLine
-        text="average"
-        value={(good - bad) / (good + neutral + bad)}
-      />
-      <StatisticLine text="positive" value={good / (good + neutral + bad)} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine
+            text="average"
+            value={(good - bad) / (good + neutral + bad) || 0}
+          />
+          <StatisticLine
+            text="positive"
+            value={good / (good + neutral + bad) || 0}
+          />
+        </tbody>
+      </table>
     </div>
   );
 };
