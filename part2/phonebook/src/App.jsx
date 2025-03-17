4286d5697f1dffb3d6f79819a35f88e8873a747d
Kroxitrock <kroxitrock@gmail.com>
2025-03-17 20:46:09 +0200

    Ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,20 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Contact from "./components/Contact.jsx";
 import ContactForm from "./components/ContactForm.jsx";
 import Filter from "./components/Filter.jsx";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ]);
+    const [persons, setPersons] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons")
+            .then(response => {
+                setPersons(response.data);
+                setFilteredPeople(response.data);
+            })
+    }, []);
+
     const [filteredPeople, setFilteredPeople] = useState(persons);
     const [newFilter, setNewFilter] = useState('');
     const [newName, setNewName] = useState('');
