daac4f1d6a386941ca52709f5fbf4549dd7ccdd7
Kroxitrock <kroxitrock@gmail.com>
2025-03-31 21:57:33 +0300

    Ex 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        req.method === 'POST' ? JSON.stringify(req.body) : '',
+    ].join(' ')
+}));
 const persons = [
     {
         "id": "1",
@@ -49,8 +60,6 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
     const newPerson = req.body;
-
-    console.log(newPerson);
 
     if (!newPerson.name || !newPerson.number) {
         return res.status(400).json({error: 'Name or number is missing'});
