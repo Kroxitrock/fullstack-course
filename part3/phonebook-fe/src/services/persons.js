97450813bc1ac023b3967b5e8c175d42a481f6c0
Kroxitrock <kroxitrock@gmail.com>
2025-04-01 21:54:15 +0300

    Ex 3.11

--- part3/phonebook-fe/src/services/persons.js
+++ part3/phonebook-fe/src/services/persons.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 export const getAllPersons = () => {
-    return axios.get("https://fullstack-course-aoco.onrender.com/persons");
+    return axios.get("https://fullstack-course-aoco.onrender.com/api/persons");
 }
 
 export const addPerson = (newPerson) => {
-    return axios.post("https://fullstack-course-aoco.onrender.com/persons", newPerson);
+    return axios.post("https://fullstack-course-aoco.onrender.com/api/persons", newPerson);
 }
 
 export const deletePerson = (id) => {
-    return axios.delete(`https://fullstack-course-aoco.onrender.com/persons/${id}`);
+    return axios.delete(`https://fullstack-course-aoco.onrender.com/api/persons/${id}`);
 }
 
 export const updatePerson = (id, newPerson) => {
-    return axios.put(`https://fullstack-course-aoco.onrender.com/persons/${id}`, newPerson);
+    return axios.put(`https://fullstack-course-aoco.onrender.com/api/persons/${id}`, newPerson);
 }
