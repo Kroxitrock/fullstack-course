675a53c0f8e3a462bd256f7a0885bcf860476a1e
Kroxitrock <kroxitrock@gmail.com>
2025-04-10 20:52:47 +0300

    Ex 4.5

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -36,3 +36,39 @@ describe('total likes', () => {
         assert.strictEqual(result, 10)
     })
 })
+
+describe('favorite blog', () => {
+    const blog1 = {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    }
+
+    const blog2 = {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 6,
+        __v: 0
+    }
+
+    test('when list is empty, returns null', () => {
+        const result = listHelper.favoriteBlog([])
+        assert.strictEqual(result, null)
+    })
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.favoriteBlog([blog1])
+        assert.deepStrictEqual(result, blog1)
+    })
+
+    test('when list has multiple blogs, returns the one with most likes', () => {
+        const result = listHelper.favoriteBlog([blog1, blog2])
+        assert.deepStrictEqual(result, blog2)
+    })
+
+})
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,8 +6,13 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, blog) => acc + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+    return blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current);
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
