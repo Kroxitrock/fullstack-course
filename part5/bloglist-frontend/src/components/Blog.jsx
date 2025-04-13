fac5bbafef64978d17d64909443e9f9e40426f8f
Kroxitrock <kroxitrock@gmail.com>
2025-04-13 13:21:30 +0300

    Ex 5.6-5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from "react";
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={(event) => {
+            event.preventDefault()
+            setView(!view)
+          }}>view</button>
+        </div>
+        {view && (
+          <div>
+            <p>{blog.url}</p>
+            <p>{blog.likes} likes<button>like</button></p>
+            <p>{blog.user.name}</p>
+          </div>
+        )}
+      </div>
+  )
+}
 
 export default Blog
